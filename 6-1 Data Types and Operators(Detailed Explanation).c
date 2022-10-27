d58a3d76639b94dd0e3bfb2d0ca9e4df0121bd31
IstiaqeAhmed <istiaqeahmed.cse@gmail.com>
2022-10-27 09:24:16 +0600

    second

---
+++ 6-1 Data Types and Operators(Detailed Explanation).c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+int main()
+{
+    // printf("%d", sizeof(char));
+    // unsigned int mean allways positive value
+    // unsigned int a = 5, b = 7;
+    // unsigned int c = a % b;
+    // printf("%ud", c);
+    //  int a = 17, b = 7;
+    //     int c = a % b;
+    //     printf("%d", c);
+    // Answer: 3
+
+    // int a = 5, b = 6;
+    // int c = a | b;
+    // printf("%d", c);
+    // bitwise operator answer:4
+
+    // logical operator
+    //  int a = 17, b = 7;
+    //  int c = (a > b) && (7 >= 4);
+    //  printf("%d", c);
+
+    // inc/decrement
+    //  uniary opration
+    int a = 17, b = 7;
+    b--;
+    printf("%d", b);
+    return 0;
+}
